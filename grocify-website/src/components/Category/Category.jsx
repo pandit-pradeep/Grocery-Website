05d052d4ba509f96dc5abcba40726ba6a8d87259
Pradeep Pandit <pp8379951@gmail.com>
2026-06-20 21:44:16 +0530

    Category Done

---
+++ grocify-website/src/components/Category/Category.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Category = () => {
+    return (
+        <section>
+
+            <div className='md:py-20 py-15 '>
+                <div className='w-fit md:top-0 mx-auto'>
+                    <h2 className='md:text-6xl text-[1.5rem]  font-bold'> <span className='text-orange-500'>Shop
+                        </span> by Category</h2>
+                    <div className='h-1  md:w-40 w-20 bg-orange-300 md:mt-4  ml-auto'></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Category
