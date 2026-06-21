790d550a749af065f8fd40f66cf0d56bac12fef6
Pradeep Pandit <pp8379951@gmail.com>
2026-06-22 00:19:20 +0530

    Process Section Start

--- grocify-website/src/components/Heading/Heading.jsx
+++ grocify-website/src/components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = (props) => {
     return (
         <div className='w-fit md:top-0 mx-auto'>
-            <h2 className='md:text-6xl text-[1.5rem]  font-bold'> <span className='text-orange-500'>{props.highlight}
+            <h2 className='text-zinc-800 md:text-6xl text-[1.5rem]  font-bold'> <span className='text-orange-500'>{props.highlight}
             </span> {props.heading}</h2>
             <div className='h-1  md:w-40 w-20 bg-orange-300 md:mt-4  ml-auto'></div>
         </div>
---
+++ grocify-website/src/components/Process/Process.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Process = () => {
+  return (
+    <section>
+        <div className='max-w-[1400px] mx-auto px-10 py-20 '>
+            <Heading highlight='Our' heading='Process' />
+
+        </div>
+    </section>
+  )
+}
+
+export default Process
