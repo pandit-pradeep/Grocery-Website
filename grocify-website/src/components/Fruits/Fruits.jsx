1b1eb90eb51de0a7ae4e13a00601b714000246ed
Pradeep Pandit <pp8379951@gmail.com>
2026-06-24 20:23:18 +0530

    Change

--- grocify-website/src/components/Fruits/Fruits.jsx
+++ grocify-website/src/components/Fruits/Fruits.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Fruits = () => {
   return (
     <div>
-      <h1>I am </h1>
+      <h1>Hello everyone my name is Pradeep Pandit and i'm from hazaribagh jharkhand india. Currently i am purshing my B.Tech from SVIET Chandigarh Punjab  And I am frontend developer and start the backend development. </h1>
     </div>
   )
 }
